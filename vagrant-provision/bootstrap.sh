651eebca34fc4d4354bfd604ea42ff6ea6c2185d
Geoff Oliver <geoff@plan8studios.com>
2015-12-05 16:18:38 -0500

    vagrant tweaks. MIGRATIONS FTW!

--- vagrant-provision/bootstrap.sh
+++ vagrant-provision/bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get upgrade -y
 
 echo "installing apache, php, mysql, etc"
-apt-get -y install apache2 php5 mysql-server memcached php5-mysql php5-memcached php5-curl
+apt-get -y install apache2 php5 memcached php5-mysql php5-memcached php5-curl php5-intl
 
 echo  "setting /var/www as vagrant's start directory"
 grep "cd /var/www" /home/vagrant/.bashrc || printf "cd /var/www\n" >> /home/vagrant/.bashrc
